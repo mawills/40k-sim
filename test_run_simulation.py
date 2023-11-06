8e62273d9d82948195fa089da8d945dc77714dee
Matt Wills <mattwillscode@gmail.com>
2023-11-05 23:02:01 -0500

    test hit_roll()

--- test_run_simulation.py
+++ test_run_simulation.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 import run_simulation
 from weapon_profile import Weapon
+from hit_roll_result import HitRollResult
 
 
 @patch("run_simulation.random.randint")
@@ -69,3 +70,186 @@ def test_attack_roll(mock_randint):
         result.append(run_simulation.attack_roll(input))
 
     assert result == [20, 30]
+
+
+@patch("run_simulation.random.randint")
+def test_hit_roll(mock_randint):
+    mock_randint.return_value = 3
+
+    inputs = [
+        [
+            Weapon(
+                "test_weapon_skill_3",
+                2,
+                3,
+                4,
+                0,
+                1,
+                10,
+                False,
+                0,
+                False,
+                False,
+                False,
+                False,
+                [],
+                [],
+                [],
+                6,
+                6,
+            ),
+            20,
+            [20, 0],
+        ],
+        [
+            Weapon(
+                "test_weapon_skill_4",
+                2,
+                4,
+                4,
+                0,
+                1,
+                10,
+                False,
+                0,
+                False,
+                False,
+                False,
+                False,
+                [],
+                [],
+                [],
+                6,
+                6,
+            ),
+            20,
+            [0, 0],
+        ],
+        [
+            Weapon(
+                "test_weapon_skill_4_torrent",
+                2,
+                4,
+                4,
+                0,
+                1,
+                10,
+                False,
+                0,
+                False,
+                True,
+                False,
+                False,
+                [],
+                [],
+                [],
+                6,
+                6,
+            ),
+            15,
+            [15, 0],
+        ],
+        [
+            Weapon(
+                "test_weapon_skill_lethal_hits_crit_3",
+                2,
+                4,
+                4,
+                0,
+                1,
+                10,
+                True,
+                0,
+                False,
+                False,
+                False,
+                False,
+                [],
+                [],
+                [],
+                3,
+                6,
+            ),
+            15,
+            [0, 15],
+        ],
+        [
+            Weapon(
+                "test_weapon_skill_lethal_hits_crit_with_torrent",
+                2,
+                4,
+                4,
+                0,
+                1,
+                10,
+                True,
+                0,
+                False,
+                True,
+                False,
+                False,
+                [],
+                [],
+                [],
+                3,
+                6,
+            ),
+            12,
+            [12, 0],
+        ],
+        [
+            Weapon(
+                "test_weapon_skill_sustained_hits_3_crit_2",
+                2,
+                4,
+                4,
+                0,
+                1,
+                1,
+                False,
+                3,
+                False,
+                False,
+                False,
+                False,
+                [],
+                [],
+                [],
+                2,
+                6,
+            ),
+            2,
+            [8, 0],
+        ],
+        [
+            Weapon(
+                "test_weapon_sustained_and_lethal",
+                2,
+                4,
+                4,
+                0,
+                1,
+                1,
+                True,
+                3,
+                False,
+                False,
+                False,
+                False,
+                [],
+                [],
+                [],
+                2,
+                6,
+            ),
+            2,
+            [6, 2],
+        ],
+    ]
+
+    results = []
+    for [weapon, num_attacks, expected] in inputs:
+        results.append([run_simulation.hit_roll(weapon, num_attacks), expected])
+
+    for [result, expected] in results:
+        assert [result.hits, result.lethal_hits] == expected
