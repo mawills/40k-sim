bb506d23beece3cc4304fb589cfdda055cb2e0ee
Matt Wills <mattwillscode@gmail.com>
2023-11-05 22:04:30 -0500

    test attack_roll()

--- test_run_simulation.py
+++ test_run_simulation.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 import run_simulation
+from weapon_profile import Weapon
 
 
 @patch("run_simulation.random.randint")
@@ -13,3 +14,58 @@ def test_dice_roll(mock_randint):
         result.append(run_simulation.dice_roll(input))
 
     assert result == [1, 1, 2, 2, 22]
+
+
+@patch("run_simulation.random.randint")
+def test_attack_roll(mock_randint):
+    mock_randint.return_value = 3
+
+    # TODO: mock config.DEFENDER_UNIT_SIZE to test Blast
+    inputs = [
+        Weapon(
+            "test_weapon_num_attacks_int",
+            2,
+            3,
+            4,
+            0,
+            1,
+            10,
+            False,
+            0,
+            False,
+            False,
+            False,
+            False,
+            [],
+            [],
+            [],
+            6,
+            6,
+        ),
+        Weapon(
+            "test_weapon_num_attacks_variable",
+            "D6",
+            4,
+            4,
+            0,
+            1,
+            10,
+            False,
+            0,
+            False,
+            True,
+            False,
+            False,
+            [],
+            [],
+            [],
+            6,
+            6,
+        ),
+    ]
+
+    result = []
+    for input in inputs:
+        result.append(run_simulation.attack_roll(input))
+
+    assert result == [20, 30]
